02ed2eaf7ab46af50986296b843a34f9d919bd53
maswhitaker <masforwhi@yahoo.com>
2014-07-17 09:04:18 -0400

    added dist folder

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,51 @@
-console.log('The Iron Yard Rocks');
+var PlanetList = Backbone.View.extend({
+  tagName: '',
+  template: _.template($('#template-target').html()),
+  render: function(){
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  },
+  initialize: function(){
+    this.render();
+  }
+});
+
+var PlanetDetail = Backbone.View.extend({
+  el: 'div',
+  initialize: function(){
+    var planetList = new PlanetList();
+  },
+  render: function(){
+    this.render();
+  }
+});
+
+var Planets = Backbone.Collection.extend({
+  model: Planet
+});
+
+var Planet = Backbone.Model.extend({
+  urlRoot: '/planets',
+defaults:{
+  name: '',
+  Url: '',
+  imgUrl: ''
+}
+});
+
+var Router = Backbone.Router.extend({
+  routes:{
+    '': 'home',
+    '/:id': 'planetDisplay'
+  }
+});
+
+var router = new Router();
+router.on("route:home",function(){
+  Planets.render();
+});
+router.on("route:planetDisplay",function(){
+  planets.render();
+});
+
+Backbone.history.start();
